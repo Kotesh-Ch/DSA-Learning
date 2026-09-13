bfa8c4fdee109fd7467e605bbef34c3605d05bc7
Kotesh-Ch <koteshch121@gmail.com>
2026-09-13 20:28:05 +0530

    Time: 43 ms (90.53%) | Memory: 45.4 MB (52.27%) - LeetSync

---
+++ 864-image-overlap/image-overlap.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        List<List<Integer>> list1 = new ArrayList<>();
+        List<List<Integer>> list2 = new ArrayList<>();
+        int n = img1.length;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(img1[i][j] == 1) {list1.add(Arrays.asList(i,j));}
+                if(img2[i][j] == 1) {list2.add(Arrays.asList(i,j));}
+            }
+        }
+
+        int[][] counts = new int[n * 2][n * 2];
+        int res = 0;
+
+        for(List<Integer> l1 : list1) {
+            for(List<Integer> l2 : list2) {
+                int dx = l2.get(0) - l1.get(0) + n;
+                int dy = l2.get(1) - l1.get(1) + n;
+
+                res = Math.max(res, ++counts[dx][dy]);
+            }
+        }
+
+        return res;
+    }
+}
