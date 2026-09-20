5ebf9f1c94d32c5c44a4870a4588a42b2ca94835
Kotesh-Ch <koteshch121@gmail.com>
2026-09-20 06:15:11 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (50.47%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int res = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            res += (123 - s.charAt(i)) * (i+1);
+        }
+
+        return res;
+    }
+}
